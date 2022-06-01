89cf2fd49268b3ed022d370a3e60f8d8116081c9
leenagyeom <leenagyeom91@gmail.com>
2022-06-01 16:10:06 +0900

    dataset path 수정(list)

--- 31_project/data_postprocessing.py
+++ 31_project/data_postprocessing.py
@@ -9,8 +9,7 @@ from torchvision.utils import save_image
 
 class MyCustomDataset(Dataset):
     def __init__(self, path, transform = None):
-        # ./preprocessing\\dragon_90.png
-        self.all_data = glob.glob(os.path.join(path, '*.png'))
+        self.all_data = path
         self.transform = transform
 
     def __getitem__(self, index):
@@ -21,13 +20,13 @@ class MyCustomDataset(Dataset):
             image = self.transform(image)
             # print(image.shape)
             # exit()
-        data_split = data_path.split('\\')
-        data_labels = data_split[1].split('.')[0].split('_')[0]
+        data_split = data_path.split('/')
+        data_labels = data_split[1]
 
         data_label = 0
         if data_labels == 'mango':
             data_label = 0
-        elif data_labels == 'dragon':
+        elif data_labels == 'dragon_fruit':
             data_label = 1
         elif data_labels == 'lychee':
             data_label = 2
@@ -46,19 +45,40 @@ torchvision_transform = transforms.Compose([
     transforms.ToTensor()]
 )
 
+# 이미지 경로 합치기
+
+image_dir = list()
+dir_durian = os.listdir('./durian')
+dir_dragon = os.listdir('./dragon_fruit')
+dir_mango = os.listdir('./mango')
+dir_lychee = os.listdir('./lychee')
+
+for img in dir_durian:
+    path_durian = './durian/'+img
+    image_dir.append(path_durian)
+for img in dir_dragon:
+    path_dragon = './dragon_fruit/' + img
+    image_dir.append(path_dragon)
+for img in dir_mango:
+    path_mango = './mango/' + img
+    image_dir.append(path_mango)
+for img in dir_lychee:
+    path_lychee = './lychee/' + img
+    image_dir.append(path_lychee)
+
+
 # train, valid, test 데이터 split
-# train : 550 / valid : 62 / test : 68
 dataset = MyCustomDataset(
-    path="./preprocessing/",
+    path=image_dir,
     transform = torchvision_transform
 )
+
 tv_data,test = train_test_split(dataset, test_size=0.1, shuffle=True)
 train,valid = train_test_split(tv_data, test_size=0.1, shuffle=True)
 
 train_dataloader = DataLoader(train, batch_size=1, shuffle=False)
 valid_dataloader = DataLoader(valid, batch_size=1, shuffle=False)
 test_dataloader = DataLoader(test, batch_size=1, shuffle=False)
-
 
 # 이미지, 라벨 출력 확인
 for (n, v, t) in zip(train_dataloader, valid_dataloader, test_dataloader):
@@ -73,13 +93,13 @@ os.makedirs('./dataset/valid', exist_ok=True)
 os.makedirs('./dataset/test', exist_ok=True)
 
 for train in train_dataloader:
-    n_filename = train[1][0].split('\\')[1]
-    save_image(train[0][0], f'./dataset/train/{n_filename}')
+    filename = train[1][0].split('/')[2]
+    save_image(train[0][0], f'./dataset/train/{filename}')
 
 for valid in valid_dataloader:
-    v_filename = valid[1][0].split('\\')[1]
-    save_image(valid[0][0], f'./dataset/valid/{v_filename}')
+    filename = valid[1][0].split('/')[2]
+    save_image(valid[0][0], f'./dataset/valid/{filename}')
 
 for test in test_dataloader:
-    t_filename = test[1][0].split('\\')[1]
-    save_image(test[0][0], f'./dataset/test/{t_filename}')
+    filename = test[1][0].split('/')[2]
+    save_image(test[0][0], f'./dataset/test/{filename}')
